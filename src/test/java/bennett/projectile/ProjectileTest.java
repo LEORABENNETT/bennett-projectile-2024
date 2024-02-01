45e297df6f9f83539f20a0abb90bc6aa70deccf4
leorabennett <lbennett9@student.touro.edu>
2024-01-31 19:46:24 -0500

    projectile homework

---
+++ src/test/java/bennett/projectile/ProjectileTest.java
@@ -0,0 +1,47 @@
+package bennett.projectile;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectileTest {
+
+    @Test
+    void getX() {
+        // given
+        Projectile projectile = new Projectile(31, 20);
+        projectile.setSeconds(2.7);
+
+        // when
+        double actual = projectile.getX();
+
+        // then
+        assertEquals(46.28, actual, 0.01);
+    }
+
+    @Test
+    public void getY() {
+        // given
+        Projectile projectile = new Projectile(31, 20);
+        projectile.setSeconds(2.7);
+
+        // when
+        double actual = projectile.getY();
+
+        // then
+        assertEquals(-7.90, actual, 0.01);
+    }
+
+    @Test
+    public void getApexTime() {
+        // given
+        Projectile projectile = new Projectile(31, 20);
+        projectile.setSeconds(2.7);
+
+        // when
+        double actual = projectile.getApexTime();
+
+        // then
+        assertEquals(1.05, actual, .01);
+    }
+}
