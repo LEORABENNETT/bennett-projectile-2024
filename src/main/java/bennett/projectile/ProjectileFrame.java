1ccc3a0058a0e49f649c973c7a1089af50942b41
leorabennett <lbennett9@student.touro.edu>
2024-02-21 18:22:39 -0500

    fixed the order to make it look right

--- src/main/java/bennett/projectile/ProjectileFrame.java
+++ src/main/java/bennett/projectile/ProjectileFrame.java
@@ -4,16 +4,21 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ProjectileFrame extends JFrame {
-
     static final int ANGLE_MIN = 0;
     static final int ANGLE_MAX = 90;
     static final int ANGLE_INIT = 45;
+    private final JSlider angleSlider;
+    private final JTextField velocityField;
+    private final JTextField secondsField;
+    private final JLabel calculatedLabelX;
+    private final JLabel calculatedLabelY;
+    private final JLabel peakY;
+    private final JLabel interceptX;
 
     public ProjectileFrame() {
         setSize(400, 600);
@@ -23,82 +28,64 @@ public class ProjectileFrame extends JFrame {
         setLayout(new GridLayout(8, 2));
         JLabel velocityLabel = new JLabel("Velocity");
         add(velocityLabel);
+
+        velocityField = new JTextField();
+        add(velocityField);
+
         JLabel angleLabel = new JLabel("Angle");
         add(angleLabel);
 
-        JSlider angleSlider = new JSlider(JSlider.HORIZONTAL,
+         angleSlider = new JSlider(JSlider.HORIZONTAL,
                 ANGLE_MIN, ANGLE_MAX, ANGLE_INIT);
-
 
         angleSlider.setMajorTickSpacing(10);
         angleSlider.setMinorTickSpacing(5);
         angleSlider.setPaintTicks(true);
         angleSlider.setPaintLabels(true);
 
+        add(angleSlider);
+
         JLabel secondsLabel = new JLabel("Seconds");
         add(secondsLabel);
+        secondsField = new JTextField();
+        add(secondsField);
+
         JLabel labelX = new JLabel("X");
         add(labelX);
+        calculatedLabelX = new JLabel();
+        add(calculatedLabelX);
+
         JLabel labelY = new JLabel("Y");
         add(labelY);
-
-        JTextField velocityField = new JTextField();
-        add(velocityField);
-        //JTextField angleField = new JTextField();
-        JTextField secondsField = new JTextField();
-        add(secondsField);
-        JLabel calculatedLabelX = new JLabel();
-        add(calculatedLabelX);
-        JLabel calculatedLabelY = new JLabel();
+        calculatedLabelY = new JLabel();
         add(calculatedLabelY);
+
+        JLabel peakLabelY = new JLabel("Peak Y");
+        add(peakLabelY);
+        peakY = new JLabel();
+        add(peakY);
+
+        JLabel interceptLabelX = new JLabel("Intercept X");
+        add(interceptLabelX);
+        interceptX = new JLabel();
+        add(interceptX);
+
         JLabel empty = new JLabel();
         add(empty);
         JButton calculateButton = new JButton("Calculate");
         add(calculateButton);
-        JLabel peakLabelY = new JLabel("Peak Y");
-        add(peakLabelY);
-        JLabel interceptLabelX = new JLabel("Intercept X");
-        add(interceptLabelX);
-        JLabel peakY = new JLabel();
-        add(peakY);
-        JLabel interceptX = new JLabel();
-        add(interceptX);
-
-
-        add(angleSlider);
-        //add(angleField);
 
         velocityField.getDocument().addDocumentListener(new SimpleDocumentListener() {
             @Override
             public void update(DocumentEvent e) {
-                Projectile projectile = new Projectile(
-                        Double.parseDouble(velocityField.getText()),
-                        Double.parseDouble(String.valueOf(angleSlider.getValue()))
-                );
-                projectile.setSeconds(
-                        Double.parseDouble(secondsField.getText())
-                );
-                calculatedLabelX.setText(Double.toString(projectile.getX()));
-                calculatedLabelY.setText(Double.toString(projectile.getY()));
-                peakY.setText(Double.toString(projectile.getPeakY()));
-                interceptX.setText(Double.toString(projectile.getInterceptX()));
+                projectileAction();
             }
         });
 
         angleSlider.addChangeListener(new ChangeListener() {
             @Override
             public void stateChanged(ChangeEvent e) {
-                Projectile projectile = new Projectile(
-                        angleSlider.getValue(),
-                        Double.parseDouble(String.valueOf(velocityField.getX()))
-                );
-                projectile.setSeconds(
-                        Double.parseDouble(secondsField.getText())
-                );
-                calculatedLabelX.setText(Double.toString(projectile.getX()));
-                calculatedLabelY.setText(Double.toString(projectile.getY()));
-                peakY.setText(Double.toString(projectile.getPeakY()));
-                interceptX.setText(Double.toString(projectile.getInterceptX()));
+                projectileAction();
             }
         });
 
@@ -106,19 +93,23 @@ public class ProjectileFrame extends JFrame {
         calculateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Projectile projectile = new Projectile(
-                        angleSlider.getValue(),
-                        Double.parseDouble(String.valueOf(velocityField.getX()))
-                );
-                projectile.setSeconds(
-                        Double.parseDouble(secondsField.getText())
-                );
-                calculatedLabelX.setText(Double.toString(projectile.getX()));
-                calculatedLabelY.setText(Double.toString(projectile.getY()));
-                peakY.setText(Double.toString(projectile.getPeakY()));
-                interceptX.setText(Double.toString(projectile.getInterceptX()));
+                projectileAction();
             }
         });
 
+    }
+
+    private void projectileAction() {
+        Projectile projectile = new Projectile(
+                angleSlider.getValue(),
+                Double.parseDouble(String.valueOf(velocityField.getX()))
+        );
+        projectile.setSeconds(
+                Double.parseDouble(secondsField.getText())
+        );
+        calculatedLabelX.setText(Double.toString(projectile.getX()));
+        calculatedLabelY.setText(Double.toString(projectile.getY()));
+        peakY.setText(Double.toString(projectile.getPeakY()));
+        interceptX.setText(Double.toString(projectile.getInterceptX()));
     }
 }
